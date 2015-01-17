54e83b98341f174e76bc0f67c842b519df170470
patrickgrey <websemantic@yahoo.co.uk>
2015-01-17 00:04:52 +0000

    Got off canvas menu working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,28 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+(function (win, doc) {
+    'use strict';
+    if (!win.addEventListener) {
+        return;
+    }
+    var linkclass = 'pc-menu-toggle',
+        activeclass = 'active',
+        enhanceclass = 'cutsthemustard',
+        toggleClassName = function (element, toggleClass) {
+            var reg = new RegExp('(\\s|^)' + toggleClass + '(\\s|$)');
+            if (!element.className.match(reg)) {
+                element.className += ' ' + toggleClass;
+            } else {
+                element.className = element.className.replace(reg, '');
+            }
+        },
+        navListener = function (ev) {
+            ev = ev || win.event;
+            var target = ev.target || ev.srcElement;
+            if (target.className.indexOf(linkclass) !== -1) {
+                ev.preventDefault();
+                toggleClassName(doc.body, activeclass);
+            }
+        };
+    doc.documentElement.className += ' ' + enhanceclass;
+    doc.addEventListener('click', navListener, false);
+}(this, this.document));
