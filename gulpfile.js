a8c8bedbf75d77f28001c1305ce7e8f69af8da8a
patrickgrey <websemantic@yahoo.co.uk>
2015-01-28 09:47:59 +0000

    Moved cut mustard menu up

--- gulpfile.js
+++ gulpfile.js
@@ -58,8 +58,8 @@ gulp.task('fonts', function () {
 gulp.task('extras', function () {
   return gulp.src([
     'app/*.*',
-    '!app/*.html',
-    'node_modules/apache-server-configs/dist/.htaccess'
+    '!app/*.html'//,
+    //'node_modules/apache-server-configs/dist/.htaccess'
   ], {
     dot: true
   }).pipe(gulp.dest('dist'));
